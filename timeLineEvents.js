a48796df9ccbc0886cd7b40958288b442ed93509
Loïc <loic.dutrieux@gmail.com>
2015-11-08 20:31:37 +0100

    Started project

---
+++ timeLineEvents.js
@@ -0,0 +1,50 @@
+var timeEvents = {
+	// "title": {
+ //            "text": {
+ //                "headline": "Eras Test",
+ //                "text":     "Events in the 7th century from https://en.wikipedia.org/wiki/Timeline_of_the_Middle_Ages"
+ //            }
+ //    },
+
+    "events": [
+        {
+            "start_date": {
+                "year":			"648"
+            },
+            "end_date": {
+                "year":			"652"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Roman-Persian_Wars"
+            },
+            "background": {
+				"opacity":"50",
+                "url": "https://upload.wikimedia.org/wikipedia/commons/a/a2/HumiliationValerianusHolbein.jpg"
+            },
+            "text": {
+                "headline": "Last great Roman-Persian War.",
+                "text": "Long conflict leaves both empires exhausted and unable to cope with the newly united Arab armies under Islam in the 630s"
+            },
+            "coords" : {
+                "lng": 20,
+                "lat": 54
+            },
+        },
+        {
+            "start_date": {
+                "year":			"660"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/North_South_States_Period"
+            },
+            "text": {
+                "headline": "North South States Period begins in Korea",
+                "text": "Silla and Balhae coexisted in the south and north of the peninsula, respectively, until 926"
+            },
+            "coords" : {
+                "lng": 22,
+                "lat": 52
+            },
+        }
+    ]
+}
