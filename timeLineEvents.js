1128320d08f06be3874a8f8662b5474bdd71368a
Loïc <loic.dutrieux@gmail.com>
2015-11-08 22:31:23 +0100

    Added a few more events to map

--- timeLineEvents.js
+++ timeLineEvents.js
@@ -1,24 +1,18 @@
 var timeEvents = {
-	// "title": {
- //            "text": {
- //                "headline": "Eras Test",
- //                "text":     "Events in the 7th century from https://en.wikipedia.org/wiki/Timeline_of_the_Middle_Ages"
- //            }
- //    },
 
-    "events": [
+        "events": [
         {
             "start_date": {
-                "year":			"648"
+                "year":         "602"
             },
             "end_date": {
-                "year":			"652"
+                "year":         "629"
             },
             "media": {
                 "url": "https://en.wikipedia.org/wiki/Roman-Persian_Wars"
             },
             "background": {
-				"opacity":"50",
+                "opacity":"50",
                 "url": "https://upload.wikimedia.org/wikipedia/commons/a/a2/HumiliationValerianusHolbein.jpg"
             },
             "text": {
@@ -32,7 +26,270 @@ var timeEvents = {
         },
         {
             "start_date": {
-                "year":			"660"
+                "year":         "604"
+            },
+            "end_date": {
+                "year":         "609"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Grand_Canal_(China)"
+            },
+            "background": {
+                "opacity":"50",
+                "url": "https://upload.wikimedia.org/wikipedia/commons/a/ad/Sui_Wendi_Tang.jpg"
+            },
+            "text": {
+                "headline": "Grand Canal in China is fully completed",
+                "text": "Its main role throughout its history was the transport of grain to the capital."
+            },
+            "coords": {
+                "lng": 105 ,
+                "lat": 30
+            },
+        },
+
+        {
+            "start_date": {
+                "year":         "622",
+                "month":        "08",
+                "day":          "09"
+            },
+            "end_date": {
+                "year":         "622",
+                "month":        "08",
+                "day":          "23"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Hijra_(Islam)"
+            },
+            "text": {
+                "headline": "Muhammad Migrates from Mecca to Medina",
+                "text": "Event will have designated first year of the Islamic Calendar, as Anno Hegirae."
+            },
+            "coords": {
+                "lng": 39.8,
+                "lat": 21.4
+            },
+        },
+        {
+            "start_date": {
+                "year":         "626"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Siege_of_Constantinople_(626)"
+            },
+            "background": {
+                "opacity":"50",
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Moldovita_murals_2010_16.jpg/1024px-Moldovita_murals_2010_16.jpg"
+            },
+            "text": {
+                "headline": "Joint Persian-Avar-Slav Siege of Constantinople saved",
+                "text": "Avar power broken and Persians henceforth on the defensive"
+            },
+            "coords": {
+                "lng": 29,
+                "lat": 41
+            },
+        },
+        {
+            "start_date": {
+                "year":         "627",
+                "month":        "12",
+                "day":          "12"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Battle_of_Nineveh_(627)"
+            },
+            "background": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Piero_della_Francesca_021.jpg/1024px-Piero_della_Francesca_021.jpg"
+            },
+            "text": {
+                "headline": "Battle of Nineveh",
+                "text": "The Byzantines, under Heraclius, crush the Persians."
+            },
+            "coords": {
+                "lng": 43,
+                "lat": 36
+            },
+        },
+        {
+            "start_date": {
+                "year":         "633"
+            },
+            "end_date": {
+                "year":         "634"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Battle_of_Heavenfield"
+            },
+            "text": {
+                "headline": "Battle of Heavenfield",
+                "text": "Northumbrian army under Oswald defeat Welsh army."
+            },
+            "coords": {
+                "lng": 3,
+                "lat": 53
+            },
+        },
+        {
+            "start_date": {
+                "year":         "641"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Battle_of_Nihaw%C4%81nd"
+            },
+            "background": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Castle_Nahavend_by_Eug%C3%A8ne_Flandin.jpg/1024px-Castle_Nahavend_by_Eug%C3%A8ne_Flandin.jpg"
+            },
+            "text": {
+                "headline": "Battle of Nehawand",
+                "text": "Muslims conquer Persia."
+            },
+            "coords": {
+                "lng": 48,
+                "lat": 34
+            },
+        },
+        {
+            "start_date": {
+                "year":         "643"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Alexandria"
+            },
+            "text": {
+                "headline": "Arab Army led by Omar Ibn Al-'Aas takes Alexandria",
+                "text": ""
+            },
+            "coords": {
+                "lng": 30,
+                "lat": 31
+            }
+        },
+        {
+            "start_date": {
+                "year":         "645"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Soga_clan"
+            },
+            "text": {
+                "headline": "In Japan, the Soga clan falls",
+                "text": "This initiates a period of imitation of Chinese culture, The Nara period begins a year later."
+            },
+            "coords": {
+                "lng": 140,
+                "lat": 36
+            },
+        },
+        {
+            "start_date": {
+                "year":         "650"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Balkans"
+            },
+            "text": {
+                "headline": "Slav occupation of Balkans complete.",
+                "text": ""
+            },
+            "coords": {
+                "lng": 18,
+                "lat": 44
+            },
+        },
+        {
+            "start_date": {
+                "year":         "663"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Synod_of_Whitby"
+            },
+            "text": {
+                "headline": "Synod of Whitby",
+                "text": "Roman Christianity triumphs over Celtic Christianity in England."
+            },
+            "coords": {
+                "lng": 51,
+                "lat": 0
+            },
+        },
+        {
+            "start_date": {
+                "year":         "674"
+            },
+            "end_date": {
+                "year":         "678"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Siege_of_Constantinople_(674)"
+            },
+            "background": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Greekfire-madridskylitzes1.jpg/1024px-Greekfire-madridskylitzes1.jpg"
+            },
+            "text": {
+                "headline": "First Arab siege of Constantinople",
+                "text": "First time Islamic armies defeated, forestalling Islamic conquest of Europe."
+            },
+            "coords": {
+                "lng": 29,
+                "lat": 41
+            },
+        },
+        {
+            "start_date": {
+                "year":         "681"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/First_Bulgarian_Empire"
+            },
+            "background": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Krum1.jpg/782px-Krum1.jpg"
+            },
+            "text": {
+                "headline": "Establishment of the Bulgarian Empire",
+                "text": "A country with great influence in the European history in the Middle Ages."
+            },
+            "coords": {
+                "lng": 25.5,
+                "lat": 42.7
+            },
+        },
+        {
+            "start_date": {
+                "year":         "687"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Battle_of_Tertry"
+            },
+            "text": {
+                "headline": "Battle of Tertry",
+                "text": "Established Pepin of Herstal as mayor over the entire realms of Neustria and Austrasia, which further dwindled Merovingian power."
+            },
+            "coords": {
+                "lng": 3,
+                "lat": 50
+            },
+        },
+        {
+            "start_date": {
+                "year":         "698"
+            },
+            "media": {
+                "url": "https://en.wikipedia.org/wiki/Carthage"
+            },
+            "text": {
+                "headline": "Arab army takes Carthage",
+                "text": "End of Byzantine rule in North Africa"
+            },
+            "coords": {
+                "lng": 29,
+                "lat": 41
+            },
+        },
+        {
+            "start_date": {
+                "year":         "698"
             },
             "media": {
                 "url": "https://en.wikipedia.org/wiki/North_South_States_Period"
